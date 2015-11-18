c6fa2318b21dfe78818211ede8100fcedc94f973
powolnymarcel <powolnymarcel@gmail.com>
2015-11-18 16:04:22 +0100

    commit

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 var express = require('express');
-var app = express();
+var application = express();
 var path = require('path');
-var serveur = require('http').createServer(app);
+var serveur = require('http').createServer(application);
 var io = require('socket.io').listen(serveur);
 var utilisateurs = [];
+var open = require('open');
+
+// Definition du moteur de vue
+
+application.set('views', __dirname + '/vues');
+application.set('view engine','jade');
+application.engine('jade', require('jade').__express);
+
+// Definition un chemin statique
+application.use(express.static(path.join(__dirname, 'public')));
+
+//On défini les routes
+application.get('/', function(req,res){
+	res.render('index');
+});
+
+// Demarrage du serveur
+var port = process.env.PORT || 3000;
+serveur.listen(port);
+console.log('************************************************************');
+console.log('Le serveur est execute, rendez-vous sur : localhost:' + port);
+console.log('************************************************************');
+open('http://localhost:' + port);
